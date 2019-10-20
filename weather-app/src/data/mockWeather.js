ccbb043312f421f3b4734e0a385710e0c7e77cf0
“meagancojocar” <meagan.cojocar@gmail.com>
2019-10-20 10:48:51 +0800

    Creating my weather app from scratch

--- weather-app/src/data/mockWeather.js
+++ weather-app/src/data/mockWeather.js
@@ -10,6 +10,7 @@ export const CURRENT_WEATHER = {
   },
   name: 'London' // Data is requested by location.
 };
+// full mock https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=b6907d289e10d714a6e88b30761fae22
 
 export const FORECAST_WEATHER = {
   city: { name: 'London' },
@@ -23,5 +24,5 @@ export const FORECAST_WEATHER = {
     }
   ] // Response will be 40 list items, 5 day forecast, 3 hourly.
 };
-
+// full mock: https://samples.openweathermap.org/data/2.5/forecast?id=524901&appid=b6907d289e10d714a6e88b30761fae22
 // weather icon codes: https://openweathermap.org/weather-conditions
